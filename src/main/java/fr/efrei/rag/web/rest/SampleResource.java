36c5769cc49e90f1c7d8ca2e22eefa5a153a7bda
CantinDeBrunoy <cantin.roquier@gmail.com>
2025-01-08 11:24:43 +0100

    #8 hello world

---
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -0,0 +1,14 @@
+package fr.efrei.rag.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleResource {
+
+    @GetMapping("/samples/{value}")
+    public String hello(@PathVariable(value = "value",required = false)final String value) {
+        return "Hello World " + value +" !";
+    }
+}

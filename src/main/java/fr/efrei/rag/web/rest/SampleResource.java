d3bfab70da30067445ac9c823a7d50c8f02d13fa
CantinDeBrunoy <cantin.roquier@gmail.com>
2025-01-08 11:38:19 +0100

    add dto

--- src/main/java/fr/efrei/rag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -1,5 +1,6 @@
 package fr.efrei.rag.web.rest;
 
+import fr.efrei.rag.web.dto.Sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class SampleResource {
     @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value",required = false)final String value) {
         return "Hello World " + value +" !";
+    }
+
+    @GetMapping("/samples/dto/{value}")
+    public Sample helloJson(@PathVariable(value = "value",required = false)final String value) {
+        return new Sample("Hello World " + value +" !");
     }
 }
